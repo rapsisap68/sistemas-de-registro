352024b169a314c9750c02781db90a0e6bff0efe
Ramiro Paniagua <ramiro.paniagua@gmail.com>
2024-07-13 20:01:27 -0500

    Función

--- apps.js
+++ apps.js
@@ -1,1 +1,11 @@
 alert("Estamos Sistema de Registro");
+console.log("Estamos Sistema de Registro");
+// write a function to find all images without alternate text and give them a red border
+function addRedBorderToImagesWithoutAltText() {
+    const images = document.getElementsByTagName('img');
+    for (let i = 0; i < images.length; i++) {
+        if (!images[i].alt) {
+            images[i].style.border = '2px solid red';
+        }
+    }
+}
